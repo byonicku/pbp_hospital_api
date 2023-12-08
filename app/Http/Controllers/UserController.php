21a6ab97a9cd0475008fda49ce687509991b8ad5
Nico Herlim <nicoherlim2003@gmail.com>
2023-12-08 20:33:52 +0700

    fix update make pfp gone

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -171,23 +171,6 @@ class UserController extends Controller
                     $userUpdate["profile_photo"] = "";
                 }
 
-                if($request->has('profile_photo')){
-                    $imageName = time() . '-' . $user->username . '.jpg';
-                    $imageFile = base64_decode($request->profile_photo);
-
-                    if (!file_exists(Storage::disk('public')->path('user'))) {
-                        mkdir(Storage::disk('public')->path('user'), 0777, true);
-                    }
-
-                    if (file_exists(Storage::disk('public')->path('user') . '/' . $user->profile_photo) && $user->profile_photo != "") {
-                        unlink(Storage::disk('public')->path('user') . '/' . $user->profile_photo);
-                    }
-
-                    Storage::disk('public')->put('user/' . $imageName, $imageFile);
-
-                    $userUpdate["profile_photo"] = $imageName;
-                }
-
                 $result = DB::table('users')->where('id_user', $id)->update([
                     "username" => $userUpdate["username"],
                     "email"=> $userUpdate["email"],
